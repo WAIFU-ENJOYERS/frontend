ff3312af2074e5dad568aeb25001357401d501f5
Aurorayyyyy <yanga262@gmail.com>
2023-03-05 14:19:25 +0700

    get some relating stuffs out/create homePage view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+<script lang="ts">
+// import TheWelcome from "../components/TheWelcome.vue";
+export default {
+  data() {
+    return {
+      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" }
+    };
+  }
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
+    <div>
+      <h1>
+        Waifu Edition Higher Lower
+      </h1>
+      <v-btn
+        variant="flat"
+        color="success"
+      >
+        Error
+      </v-btn>
+    </div>
   </main>
 </template>
