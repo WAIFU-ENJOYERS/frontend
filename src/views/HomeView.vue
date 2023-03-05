db38851ac88fac5bbe432f2422e23e222b9e5edc
karank85 <kkjas85@gmail.com>
2023-03-05 16:27:11 +0700

    feat: button format and nicer page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,18 +10,23 @@ export default {
 </script>
 
 <template>
-  <main>
-
-    <div>
-      <h1>
-        Waifu Edition Higher Lower
-      </h1>
-      <v-btn
-        variant="flat"
-        color="success"
-      >
-        Error
-      </v-btn>
-    </div>
-  </main>
+  <v-container fluid fill-height class="justify-center mt-16">
+      <v-sheet class="d-flex flex-column align-center">
+        <v-sheet class="flex-column justify-center ma-10">
+          <p class="text-h2">
+            Higher Lower: Waifu Edition
+          </p>
+        </v-sheet>
+        <v-sheet class="align-center ma-12">
+          <v-btn
+            rounded="xl"
+            variant="flat"
+            color="success"
+            size="x-large"
+          >
+            Play Game
+          </v-btn>
+        </v-sheet>
+      </v-sheet>
+  </v-container>
 </template>
