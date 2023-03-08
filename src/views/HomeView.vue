9dcb0e541982527dbebfc9e7ff8eaedbc6e17d41
karank85 <kkjas85@gmail.com>
2023-03-09 01:23:00 +0700

    added logo + description

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,25 @@
   <v-container fluid fill-height class="justify-center mt-16 bg-transparent">
       <v-sheet class="d-flex flex-column align-center bg-transparent">
         <v-sheet class="flex-column justify-center ma-10 bg-transparent">
-          <p class="text-h2 text-black">
-            Higher Lower: Waifu Edition
-          </p>
+          <v-img class="mx-auto align-center" :width="400" src="https://i.postimg.cc/YSHwbFQk/logo1.png"></v-img>
         </v-sheet>
+        <v-sheet class="flex-column justify-center ma-5 bg-shades-transparent">
+          <p class="align-center text-h3 text-white my-2 font-weight-bold">Which waifu has more likes ? </p>
+        </v-sheet>
+        <v-sheet class="flex-column justify-center ma-5 bg-shades-transparent">
+          <p class="text-white align-center mt-5 justify-center font-weight-bold">
+            A game where you guess if a waifu has higher or lower likes than other waifu based <br/>
+            on anime list data we collected
+          </p>
+          </v-sheet>
         <v-sheet class="align-center ma-12 bg-transparent">
           <v-btn
-            rounded="xl"
+            rounded="pill"
             variant="flat"
             color="success"
             size="x-large"
           >
-            Play Game
+            <p class="text-button font-weight-bold">Play Game</p>
           </v-btn>
         </v-sheet>
       </v-sheet>
