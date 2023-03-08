f3317a6b63aeda3bba209ba059d44d1c2d9f334a
Suphawit <suphawit.won@gmail.com>
2023-03-09 00:40:18 +0700

    integrate frotend and backend

--- src/views/S3Demo.vue
+++ src/views/S3Demo.vue
@@ -1,5 +1,30 @@
 <template>
-    <v-container>
-        <v-img src="https://i.pinimg.com/originals/54/a3/f4/54a3f4c2e67dfd351c80e55b49db6e6c.jpg"></v-img>
+    <v-container bg-transparent>
+        <v-card :class="['flat d-flex flex-column align-center justify-center bg-transparent', `elevation-${0}`]" height="100%">
+            <h1>{{ tasks.text }}</h1>
+            <v-img width="1280" :src=tasks.gif></v-img>
+        </v-card>
     </v-container>
 </template>
+<script lang="ts">
+import axios from 'axios'
+export default {
+    name: 's3demo',
+    data() {
+        return {
+            tasks: []
+        }
+    },
+    mounted() {
+        this.get_Tasks();
+    },
+    methods: {
+        get_Tasks() {
+            axios({
+                method: 'get',
+                url: ' http://localhost:8000/s3',
+            }).then(response => this.tasks = response.data);
+        }
+    }
+};
+</script>
