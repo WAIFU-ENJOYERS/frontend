6180505aa4266da5351c2f9cd69a98ab7726b96d
Suphawit <suphawit.won@gmail.com>
2023-03-17 16:59:40 +0700

    resolved merge conflict

--- components/home.vue
+++ components/home.vue
@@ -14,20 +14,3 @@ defineEmits<{
         </v-sheet>
     </v-container>
 </template>
-
-
-  
-<style>
-.bg {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  filter: blur(5px) grayscale(50%) brightness(50%);
-  top: 0;
-  left: 0;
-  background: url('https://giffiles.alphacoders.com/134/13493.gif') no-repeat center center;
-  background-size: cover;
-  background-color: black;
-  transform: scale(1.1);
-}
-</style>
