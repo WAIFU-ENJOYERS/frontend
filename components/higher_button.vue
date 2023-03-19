76db6db23286605cea13659d2ecbe37dbf3b81b2
karank85 <kkjas85@gmail.com>
2023-03-19 22:43:36 +0700

    feat: created higher or lower buttons

---
+++ components/higher_button.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-btn rounded="pill" variant="outlined" color="warning" size="x-large" @click="$emit('play')">
+      <p class="text-button font-weight-bold"><v-icon icon="mdi-menu-up" />Higher</p>
+    </v-btn>
+</template>
+
+<script setup lang="ts">
+</script>
