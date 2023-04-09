54bcb0c2c067282b7dca0b8e588811a079d16dc1
Suphawit <suphawit.won@gmail.com>
2023-04-09 17:54:43 +0700

    delete get image api

--- server/api/images/[name].get.ts
+++
@@ -1,7 +0,0 @@
-import { getImage } from "../../lib/s3"
-
-export default defineEventHandler( async (event) => {
-    const query = event.context.params.name
-    const docs = await getImage(query as string)
-    return docs
-})
