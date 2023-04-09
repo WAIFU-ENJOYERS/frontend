2d7c34baa578a3cdf165caabe0e8b27a62c40bd5
Aurorayyyyy <yanga262@gmail.com>
2023-04-09 16:54:00 +0700

    change the location of the tests and init the random tests

--- server/lib/home.test.ts
+++
@@ -1,8 +0,0 @@
-import { it, expect } from "vitest";
-import { queryRandomWaifu } from "../lib/firestore"
-it("data not null", async () => {
-    console.log(queryRandomWaifu)
-    
-    const docs = await queryRandomWaifu(`${0}`)
-    expect(docs).not.toBeNull;
-})
