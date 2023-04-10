707aff231cc32b16e10005b7d498ef1a0e6bc0d9
Aurorayyyyy <yanga262@gmail.com>
2023-04-10 21:51:44 +0700

    lintfix

--- e2e/home_page.spec.ts
+++ e2e/home_page.spec.ts
@@ -2,7 +2,9 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Game tests", () => {
   test("Play Button is work", async ({ page }) => {
-    await page.goto('https://frontend-red-phi.vercel.app/', { timeout: 100000 });
+    await page.goto("https://frontend-red-phi.vercel.app/", {
+      timeout: 100000,
+    });
     // await page.goto("http://localhost:3000/", { timeout: 100000 });
     const button = page.getByText("Play Game");
     await button.click({ timeout: 100000 });
